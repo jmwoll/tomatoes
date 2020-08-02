878e1a652d2a1a1c15e90ba3148391c367e1979f
jan <janmwoll@gmail.com>
2020-08-02 18:33:06 +0200

    initial commit. added bash script

---
+++ tomatoes.sh
@@ -0,0 +1,21 @@
+start=$SECONDS
+endat=$1
+
+function tomato {
+secondsleft=$(( $SECONDS - $start ))
+secondsleft=$(( $endat - $secondsleft ))
+minutes=$((secondsleft / 60))
+seconds=$((secondsleft % 60))
+printf "🍅🍅🍅  %02d:%02d 🍅🍅🍅" $minutes $seconds
+}
+
+duration=$(( SECONDS - start ))
+while [ $duration -lt $1 ]
+do
+duration=$(( SECONDS - start ))
+clear
+tomato
+sleep 1
+done
+
+notify-send $2
